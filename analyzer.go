c9e99cc5fea33d8d3c091ed4745a6f0c066819a3
Florian Bosdorff <dev@bosdorff.de>
2022-01-05 18:09:11 +0100

    refactor rules

--- analyzer.go
+++ analyzer.go
@@ -8,6 +8,20 @@ import (
 	"golang.org/x/tools/go/analysis"
 )
 
+type (
+	decNumChecker struct {
+		tokenMap    map[string]token.Token
+		tokenCounts map[token.Token]int
+		decOrder    []string
+		funcPoss    []funcPos
+	}
+
+	funcPos struct {
+		start token.Pos
+		end   token.Pos
+	}
+)
+
 const (
 	Name = "decorder"
 
@@ -28,6 +42,8 @@ var (
 	disableDecNumCheck        bool
 	disableDecOrderCheck      bool
 	disableInitFuncFirstCheck bool
+
+	tokens = []token.Token{token.TYPE, token.CONST, token.VAR, token.FUNC}
 )
 
 //nolint:lll
@@ -40,7 +56,7 @@ func init() {
 
 func run(pass *analysis.Pass) (interface{}, error) {
 	for _, f := range pass.Files {
-		ast.Inspect(f, runDeclNumCheck(pass))
+		ast.Inspect(f, runDeclNumAndDecOrderCheck(pass))
 
 		if !disableInitFuncFirstCheck {
 			ast.Inspect(f, runInitFuncFirstCheck(pass))
@@ -71,93 +87,110 @@ func runInitFuncFirstCheck(pass *analysis.Pass) func(ast.Node) bool {
 	}
 }
 
-//nolint:funlen,gocognit
-func runDeclNumCheck(pass *analysis.Pass) func(ast.Node) bool {
-	ts := []token.Token{token.TYPE, token.CONST, token.VAR, token.FUNC}
-
-	tsMap := map[string]token.Token{}
-	counts := map[token.Token]int{}
-	for _, t := range ts {
-		counts[t] = 0
-		tsMap[t.String()] = t
-	}
-
-	var funcPoss []struct {
-		start token.Pos
-		end   token.Pos
-	}
-
-	dos := strings.Split(decOrder, ",")
-
-	for i, do := range dos {
-		dos[i] = strings.TrimSpace(do)
-	}
-
-	check := func(t token.Token) (string, bool) {
-		for i, do := range dos {
-			if do == t.String() {
-				for j := i + 1; j < len(dos); j++ {
-					if counts[tsMap[dos[j]]] > 0 {
-						return dos[j], false
-					}
-				}
-				return "", true
-			}
-		}
-
-		return "", true
-	}
+func runDeclNumAndDecOrderCheck(pass *analysis.Pass) func(ast.Node) bool {
+	dnc := newDecNumChecker()
 
 	return func(n ast.Node) bool {
-		fn, ok := n.(*ast.FuncDecl)
+		fd, ok := n.(*ast.FuncDecl)
 		if ok {
-			funcPoss = append(funcPoss, struct {
-				start token.Pos
-				end   token.Pos
-			}{start: fn.Pos(), end: fn.End()})
-
-			counts[token.FUNC]++
-
-			if !disableDecOrderCheck {
-				o, c := check(token.FUNC)
-				if !c {
-					pass.Reportf(fn.Pos(), "%s must not be placed after %s", token.FUNC.String(), o)
-				}
-			}
-
-			return true
+			return dnc.handleFuncDec(fd, pass)
 		}
 
-		dn, ok := n.(*ast.GenDecl)
+		gd, ok := n.(*ast.GenDecl)
 		if !ok {
 			return true
 		}
 
-		for _, poss := range funcPoss {
-			if poss.start < dn.Pos() && poss.end > dn.Pos() {
-				return true
-			}
+		if dnc.isInsideFunction(gd) {
+			return true
 		}
 
 		if !disableDecNumCheck {
-			for _, t := range ts {
-				if dn.Tok == t {
-					counts[t]++
-
-					if counts[t] > 1 {
-						pass.Reportf(dn.Pos(), "multiple \"%s\" declarations are not allowed; use parentheses instead", t.String())
-					}
-				}
-			}
+			dnc.handleDecNumCheck(gd, pass)
 		}
 
 		if !disableDecOrderCheck {
-			o, c := check(dn.Tok)
-			if !c {
-				pass.Reportf(dn.Pos(), "%s must not be placed after %s", dn.Tok.String(), o)
-			}
+			dnc.handleDecOrderCheck(gd, pass)
 		}
 
 		return true
 	}
+}
+
+func newDecNumChecker() decNumChecker {
+	dnc := decNumChecker{
+		tokenMap:    map[string]token.Token{},
+		tokenCounts: map[token.Token]int{},
+		decOrder:    []string{},
+		funcPoss:    []funcPos{},
+	}
+
+	for _, t := range tokens {
+		dnc.tokenCounts[t] = 0
+		dnc.tokenMap[t.String()] = t
+	}
+
+	for _, do := range strings.Split(decOrder, ",") {
+		dnc.decOrder = append(dnc.decOrder, strings.TrimSpace(do))
+	}
+
+	return dnc
+}
+
+func (dnc decNumChecker) isToLate(t token.Token) (string, bool) {
+	for i, do := range dnc.decOrder {
+		if do == t.String() {
+			for j := i + 1; j < len(dnc.decOrder); j++ {
+				if dnc.tokenCounts[dnc.tokenMap[dnc.decOrder[j]]] > 0 {
+					return dnc.decOrder[j], false
+				}
+			}
+			return "", true
+		}
+	}
+
+	return "", true
+}
+
+func (dnc *decNumChecker) handleDecNumCheck(gd *ast.GenDecl, pass *analysis.Pass) {
+	for _, t := range tokens {
+		if gd.Tok == t {
+			dnc.tokenCounts[t]++
+
+			if dnc.tokenCounts[t] > 1 {
+				pass.Reportf(gd.Pos(), "multiple \"%s\" declarations are not allowed; use parentheses instead", t.String())
+			}
+		}
+	}
+}
+
+func (dnc decNumChecker) handleDecOrderCheck(gd *ast.GenDecl, pass *analysis.Pass) {
+	l, c := dnc.isToLate(gd.Tok)
+	if !c {
+		pass.Reportf(gd.Pos(), "%s must not be placed after %s", gd.Tok.String(), l)
+	}
+}
+
+func (dnc decNumChecker) isInsideFunction(dn *ast.GenDecl) bool {
+	for _, poss := range dnc.funcPoss {
+		if poss.start < dn.Pos() && poss.end > dn.Pos() {
+			return true
+		}
+	}
+	return false
+}
+
+func (dnc *decNumChecker) handleFuncDec(fd *ast.FuncDecl, pass *analysis.Pass) bool {
+	dnc.funcPoss = append(dnc.funcPoss, funcPos{start: fd.Pos(), end: fd.End()})
+
+	dnc.tokenCounts[token.FUNC]++
+
+	if !disableDecOrderCheck {
+		l, c := dnc.isToLate(token.FUNC)
+		if !c {
+			pass.Reportf(fd.Pos(), "%s must not be placed after %s", token.FUNC.String(), l)
+		}
+	}
+
+	return true
 }
